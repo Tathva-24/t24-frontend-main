e6f70374b1d658ac66434d05b2abebc674669a76
Lishin <lishinvs6541@gmail.com>
2024-10-12 15:34:28 +0530

    Improve responsiveness

--- tathva'24/src/app/page.js
+++ tathva'24/src/app/page.js
@@ -3,7 +3,11 @@ import Wheels from "./wheels/page";
 export default function Home() {
     return (
         <div>
-            <Wheels/>
+            <Wheels
+            num="03"
+            heading="About"
+            text="Metal mayhem in the arena! Witness the clash of steel and sparks at RoboWars - where all the metal enthusiasts are invited to prepare their robots to battle for supremacy in the ultimate mechanical showdown and earn prizes worth 7 lakhs!"
+            />
         </div>
     );
 }
