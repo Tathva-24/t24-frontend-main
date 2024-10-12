217c9f86f730439f3e3b1fd994831eb309a6e3cd
Vinit Keshri <vinitkehri4171@gmail.com>
2024-10-12 21:05:13 +0530

    Started with image grid

--- tathva'24/src/app/components/ImageGrid/ImageGrid.jsx
+++ tathva'24/src/app/components/ImageGrid/ImageGrid.jsx
@@ -3,47 +3,23 @@ import Image from 'next/image';
 
 const ImageGrid = () => {
 
+  const one = "public/InnerPages/concert_green.webp";
+  const two = "public/InnerPages/drone_tesal";
+  const three = "public/InnerPages/expopic.webp";
+  const four = "public/InnerPages/red_car.webp";
+
   return (
-    <div className='w-full h-full flex flex-col'>
+    <div className='w-full h-full flex flex-col mx-2'>
       <div className="one flex w-full h-64 relative ratio ">
         <div className="relative w-[50%] h-full flex">
-          <Image 
-            src={'/InnerPages/concert_green.webp'} 
-            layout="fill" 
-            objectFit="cover" 
-            objectPosition="bottom" 
-            alt="Grid" 
-            className="p-5 " 
-          />
-          <Image 
-            src={'/InnerPages/concert_green.webp'} 
-            layout="fill" 
-            objectFit="cover" 
-            objectPosition="bottom" 
-            alt="Grid" 
-            className="p-5 " 
-          />
+          <img src={one} alt="" className='w-max'/>
+          <img src={two} alt="" className='w-max'/>
         </div>
       </div>
-      
       <div className="one flex w-full h-64 relative">
         <div className="relative w-[50%] h-full flex">
-          <Image 
-            src={'/InnerPages/concert_green.webp'} 
-            layout="fill" 
-            objectFit="cover" 
-            objectPosition="bottom" 
-            alt="Grid" 
-            className="p-5 " 
-          />
-          <Image 
-            src={'/InnerPages/concert_green.webp'} 
-            layout="fill" 
-            objectFit="cover" 
-            objectPosition="bottom" 
-            alt="Grid" 
-            className="p-5 " 
-          />
+        <img src={three} alt="" className='w-max'/>
+        <img src={four} alt="" className='w-max'/>
         </div>
       </div>
     </div>
