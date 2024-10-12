fab185ccdc6f77983bea0281c9cedba2d16121c9
27-aditya <kambleaditya946@gmail.com>
2024-10-13 00:33:01 +0530

    [code] finished the register button

--- tathva'24/src/app/components/RegisterButton/registerButton.jsx
+++ tathva'24/src/app/components/RegisterButton/registerButton.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import styles from './registerButton.module.css'
+import styles from './registerButton.module.css';
 
-const Button = ({ colorOne, colorTwo, colorThree, colorFour }) => {
+const Button = ({ colorOne, colorTwo, colorThree }) => {
   return (
-    <button className="border-4 border-white bg-black text-3xl font-bold p-10 rounded-3xl">
-      <span className={` text-[${colorOne}]`}>Re</span>
-      <span className={` text-[${colorTwo}]`}>gi</span>
-      <span className={` text-[${colorThree}]`}>ster</span>
-      <span className={` text-[${colorFour}]`}> now</span>
+    <button className={`border-4 border-white bg-black text-5xl font-bold p-10 rounded-[12px] md:rounded-[30px] ${styles.gta}`}>
+      <span style={{ color: colorOne }}>Re</span>
+      <span style={{ color: colorTwo }}>gi</span>
+      <span style={{ color: colorThree }}>ste</span>
+      <span style={{ color: colorTwo }}>r n</span>
+      <span style={{ color: colorOne }}>ow</span>
     </button>
   );
 };
 
 export default Button;
-    
--- tathva'24/src/app/wheels/page.js
+++ tathva'24/src/app/wheels/page.js
@@ -2,24 +2,27 @@ import React from 'react'
 import InnerDescription from '../components/InnerDescription/InnerDescription'
 import Marquee from '../components/marquee/marquee_comp'
 import ImageGrid from '../components/ImageGrid/ImageGrid'
+import Button from '../components/RegisterButton/registerButton'
 
 const page = () => {
   return (
     <div className='w-screen h-screen overflow-x-hidden'>
-        <Marquee element1={"RoboWars"} element2={"RoboWars"} color1={"red"} color2={"black"} />
+        <Marquee element1={"auto"} element2={"summit"} color1={"red"} color2={"black"} />
+        <div className='w-full px-10'>
+          <Button colorOne={"#b60203"} colorTwo={"#f89fa4"} colorThree={"#ffffff"} ></Button>
+        </div>
         <div className="w-full p-10 flex justify-between">
-  <div className='desc w-[55%]'>
-    <InnerDescription 
-      num="03" 
-      heading="About" 
-      text="Metal mayhem in the arena! Witness the clash of steel and sparks at RoboWars - where all the metal enthusiasts are invited to prepare their robots to battle for supremacy in the ultimate mechanical showdown and earn prizes worth 7 lakhs!" 
-    />
-  </div>    
-  <div className='gridImg w-[40%] h-full mx-3'>
-    <ImageGrid />
-  </div>
-</div>
-
+        <div className='desc w-[55%]'>
+          <InnerDescription 
+            num="03" 
+            heading="About" 
+            text="Metal mayhem in the arena! Witness the clash of steel and sparks at RoboWars - where all the metal enthusiasts are invited to prepare their robots to battle for supremacy in the ultimate mechanical showdown and earn prizes worth 7 lakhs!" 
+          />
+        </div>    
+        <div className='gridImg w-[40%] h-full mx-3'>
+          <ImageGrid />
+        </div>
+      </div>
     </div>
   )
 }
