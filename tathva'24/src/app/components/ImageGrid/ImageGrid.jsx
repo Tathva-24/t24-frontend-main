c282944ce9e18be42e3da1648b1ff6b2cdb60188
arunnats <arunnats2004@gmail.com>
2024-10-13 14:51:24 +0530

    [fix] height issue

--- tathva'24/src/app/components/ImageGrid/ImageGrid.jsx
+++ tathva'24/src/app/components/ImageGrid/ImageGrid.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import Image from 'next/image';
+import React from "react";
+import Image from "next/image";
 
 const ImageGrid = () => {
   const one = "/InnerPages/concert_green.webp";
@@ -10,19 +10,19 @@ const ImageGrid = () => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 w-full mx-auto">
       <div className="relative border-4 rounded-[12px] md:rounded-[30px] overflow-hidden">
-        <img src={one} alt="" className="object-cover w-full h-64"/>
+        <img src={one} alt="" className="object-cover w-full h-64" />
       </div>
       <div className="relative border-4 rounded-[12px] md:rounded-[30px] overflow-hidden">
-        <img src={two} alt="" className="object-cover w-full h-64"/>
+        <img src={two} alt="" className="object-cover w-full h-64" />
       </div>
       <div className="relative border-4 rounded-[12px] md:rounded-[30px] overflow-hidden">
-        <img src={three} alt="" className="object-cover w-full h-64"/>
+        <img src={three} alt="" className="object-cover w-full h-64" />
       </div>
       <div className="relative border-4 rounded-[12px] md:rounded-[30px] overflow-hidden">
-        <img src={four} alt="" className="object-cover w-full h-64"/>
+        <img src={four} alt="" className="object-cover w-full h-64" />
       </div>
     </div>
   );
-}
+};
 
 export default ImageGrid;
