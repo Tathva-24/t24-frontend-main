b670ffb81625b580b03b42868b716791f955bfe9
27-aditya <kambleaditya946@gmail.com>
2024-10-14 22:59:57 +0530

    [code] made registerButton somewhat reponsive

--- tathva'24/src/app/components/RegisterButton/registerButton.jsx
+++ tathva'24/src/app/components/RegisterButton/registerButton.jsx
@@ -3,12 +3,13 @@ import styles from './registerButton.module.css';
 
 const Button = ({ colorOne, colorTwo, colorThree }) => {
   return (
-    <button className={`border-4 border-white bg-black text-5xl font-bold p-10 rounded-[12px] md:rounded-[30px] ${styles.gta}`}>
-      <span style={{ color: colorOne }}>Re</span>
-      <span style={{ color: colorTwo }}>gi</span>
-      <span style={{ color: colorThree }}>ste</span>
-      <span style={{ color: colorTwo }}>r n</span>
-      <span style={{ color: colorOne }}>ow</span>
+    <button className={`border-4 border-white bg-black text-5xl font-bold sm:p-2  lg:p-10 rounded-[12px] md:rounded-[30px] ${styles.gta} transition-all`}>
+        <span style={{ color: colorOne, fontSize: 'clamp(1rem, 5vw, 3rem)'}}>Re</span>
+        <span style={{ color: colorTwo, fontSize: 'clamp(1rem, 5vw, 3rem)' }}>gi</span>
+        <span style={{ color: colorThree, fontSize: 'clamp(1rem, 5vw, 3rem)' }}>ste</span>
+        <span style={{ color: colorTwo, fontSize: 'clamp(1rem, 5vw, 3rem)' }}>r </span>
+        <span style={{ color: colorTwo, fontSize: 'clamp(1rem, 5vw, 3rem)' }}>n</span>
+        <span style={{ color: colorOne, fontSize: 'clamp(1rem, 5vw, 3rem)' }}>ow</span>
     </button>
   );
 };
