3c675bafbf7354b99e86eec88a57d678f972ee24
Rohith <rohithofficial10@gmail.com>
2024-10-14 21:35:39 +0530

    added prizesboxcomp still working on it

---
+++ tathva'24/src/app/components/PrizesBox/PrizesWorthBox.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './PrizesWorthBox.css';
+
+const PrizesWorthBox = () => {
+    return (
+        <div className={`prize flex flex-col justify-center py-8 px-8 w-[40%] bg-black`}>
+            <div className="head text-5xl text-white flex justify-center border-b-2 py-8 font-pricedown">Prizes Worth</div>
+            <div className="bod py-8 pb-0 text-white txt-2xl flex justify-between">
+                <div className="date">
+                    <span className="font-presstart">Date:</span>
+                    <span className="font-presstart" style={{ color: 'var(--neon)' }}>28/29 Oct</span>
+                </div>
+                <div className="rt whitespace-normal w-[44%] flex justify-between">
+                    <span className="venue font-presstart w-[30%]">Venue:</span>
+                    <span className="place font-presstart text-right inline-block w-[60%]" style={{ color: 'var(--neon)' }}>Football ground</span>
+                </div>
+            </div>
+            <div className="flex justify-center space-x-1">
+                <div className="h-4 w-4 border-2 border-white"></div>
+                <div className="h-4 w-4 border-2 border-white"></div>
+                <div className="h-4 w-4 border-2 border-white"></div>
+            </div>
+        </div>
+    );
+};
+
+export default PrizesWorthBox;
--- tathva'24/src/app/page.js
+++ tathva'24/src/app/page.js
@@ -1,16 +1,20 @@
 import styles from "./page.module.css";
 import FooterMobile from './components/FooterMobile/FooterMobile';
 import FooterDesktop from "./components/FooterDesktop/FooterDesktop";
+import PrizesWorthBox from "./components/PrizesBox/PrizesWorthBox";
 // import FooterPC from "./footerpc";
 
 const Page = () => {
   return (
+    <>
+    
     <div className="h-[200vh] relative">
       <div className="footer h-max w-full absolute bottom-0 left-0">
         <FooterDesktop />
         <FooterMobile />
       </div>
    </div>
+   </>  
   );
 };
 
