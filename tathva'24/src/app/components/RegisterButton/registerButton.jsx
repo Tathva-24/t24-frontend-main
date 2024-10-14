e639b5bff5503ca4cdd1bc22ffbaa15118a317ba
27-aditya <kambleaditya946@gmail.com>
2024-10-14 23:08:03 +0530

    [code] made innerDescription reponsive

--- tathva'24/src/app/components/RegisterButton/registerButton.jsx
+++ tathva'24/src/app/components/RegisterButton/registerButton.jsx
@@ -3,7 +3,7 @@ import styles from './registerButton.module.css';
 
 const Button = ({ colorOne, colorTwo, colorThree }) => {
   return (
-    <button className={`border-4 border-white bg-black text-5xl font-bold sm:p-2  lg:p-10 rounded-[12px] md:rounded-[30px] ${styles.gta} transition-all`}>
+    <button className={`border-4 border-white bg-black text-5xl font-bold sm:py-2  lg:p-10 rounded-[12px] md:rounded-[30px] ${styles.gta} transition-all`}>
         <span style={{ color: colorOne, fontSize: 'clamp(1rem, 5vw, 3rem)'}}>Re</span>
         <span style={{ color: colorTwo, fontSize: 'clamp(1rem, 5vw, 3rem)' }}>gi</span>
         <span style={{ color: colorThree, fontSize: 'clamp(1rem, 5vw, 3rem)' }}>ste</span>
