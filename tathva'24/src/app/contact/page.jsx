5baf526c9ece29c672f5a2cfc4bc964a81689c37
ARUNKRISH11 <krish.tech.arun.11@gmail.com>
2024-10-18 00:20:37 +0530

    border

--- tathva'24/src/app/contact/page.jsx
+++ tathva'24/src/app/contact/page.jsx
@@ -4,70 +4,76 @@ import styles from "../../app/contact/contact.module.css";
 function page() {
   return (
     <div>
-      <div className=" pt-10 pb-10 p-4 bg-black">
-        <div className="p-2 flex justify-center lg:w-1/2 lg:float-start ">
-          <img src="game.svg" className="w-[400px] px-10" alt="" />
-        </div>
-        <div className="p-2 flex justify-center lg:w-1/2  ">
-          <img src="over.svg" className="w-[400px] px-10" alt="" />
-        </div>
-      </div>
-      <div className="bg-black text-white lg:w-1/2 lg:float-start">
-        <h1 className="p-8 flex justify-center">
-          <img src="contact.svg" className="w-[400px] " alt="contact" />
-        </h1>
-        <div className={styles.textfont}>
-          <div className="flex justify-center pb-6">
-            Lakshmi Priya S <br />
-            +91 62829621911
+      <div className="">
+        <div className=" pt-10 pb-10 p-4 bg-black border-4">
+          <div className="p-2 flex justify-center lg:w-1/2 lg:float-start ">
+            <img src="game.svg" className="w-[400px] px-10" alt="" />
           </div>
-          <div className="flex justify-center pb-6">
-            Nithin C Lal <br />
-            +91 94470512231
-          </div>
-          <div className="flex justify-center pb-6">
-            Aditya Suryawanshi <br />
-            +91 86683196471
-          </div>
-          <div className="flex justify-center pb-6">
-            Polu Koushik Reddy <br />
-            +91 63014029841
+          <div className="p-2 flex justify-center lg:w-1/2  ">
+            <img src="over.svg" className="w-[400px] px-10" alt="" />
           </div>
         </div>
       </div>
-      <div className="bg-black  text-white ">
-        <h1 className="p-8 flex justify-center">
-          <img src="message.svg" className="w-[400px]" alt="message" />
-        </h1>
-        <div className="flex justify-center pb-6">
-          <div className={styles.formfont}>
-            <form >
-              <input
-                type="text"
-                className="placeholder-black ps-2 text-black
+      <div className="">
+        <div className="bg-black text-white lg:w-1/2 lg:h-full lg:float-start border-4">
+          <h1 className="p-8 flex justify-center">
+            <img src="contact.svg" className="w-[400px] " alt="contact" />
+          </h1>
+          <div className={styles.textfont}>
+            <div className="flex justify-center pb-6">
+              Lakshmi Priya S <br />
+              +91 62829621911
+            </div>
+            <div className="flex justify-center pb-6">
+              Nithin C Lal <br />
+              +91 94470512231
+            </div>
+            <div className="flex justify-center pb-6">
+              Aditya Suryawanshi <br />
+              +91 86683196471
+            </div>
+            <div className="flex justify-center pb-6">
+              Polu Koushik Reddy <br />
+              +91 63014029841
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="">
+        <div className="bg-black  text-white border-4 lg:float-end lg:w-1/2 lg:h-full">
+          <h1 className="p-8 flex justify-center">
+            <img src="message.svg" className="w-[400px]" alt="message" />
+          </h1>
+          <div className="flex justify-center pb-6">
+            <div className={styles.formfont}>
+              <form>
+                <input
+                  type="text"
+                  className="placeholder-black ps-2 text-black
                 border-4 border-[#3967BF] border-r-8 border-b-8 mb-6 flex"
-                placeholder="enter name"
-              />
-              <input
-                type="text"
-                className="placeholder-black ps-2 text-black
+                  placeholder="enter name"
+                />
+                <input
+                  type="text"
+                  className="placeholder-black ps-2 text-black
                 border-4 border-[#3967BF] border-r-8 border-b-8 mb-6 flex"
-                placeholder="enter name"
-              />
-              <input
-                type="text"
-                className="placeholder-black ps-2 pb-48 text-black
+                  placeholder="enter name"
+                />
+                <input
+                  type="text"
+                  className="placeholder-black ps-2 pb-48 text-black
                 border-4 border-[#3967BF] border-r-8 border-b-8 mb-6 flex"
-                placeholder="enter name"
-              />
-              <div className="flex justify-center hover:cursor-pointer">
-                <div className={styles.submitbg}>
-                  <submit className="flex justify-center align-middle text-[#FFA400]" >
-                    submit
-                  </submit>
+                  placeholder="enter name"
+                />
+                <div className="flex justify-center hover:cursor-pointer">
+                  <div className={styles.submitbg}>
+                    <submit className="flex justify-center align-middle text-[#FFA400]">
+                      submit
+                    </submit>
+                  </div>
                 </div>
-              </div>
-            </form>
+              </form>
+            </div>
           </div>
         </div>
       </div>
