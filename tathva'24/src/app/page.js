268fe2fe51f5514854d518ed202de5d925ad1f07
mihirpa18 <punchithayamedhamihir@gmail.com>
2024-10-18 19:31:47 +0530

    added responsive navbar

---
+++ tathva'24/src/app/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from './navbar/Navbar'
+import Navbar_mob from './navbar/navbar_mob'
+
+export default function page() {
+  return (
+    <div>
+        <Navbar/>
+    </div>
+  )
+}
